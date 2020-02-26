23345233ab48c9fefd5495d8167c328ec8ef1437
nagubash <ngurshareef123@gmail.com>
2020-02-26 22:13:04 +0530

    first commit from eclipse

---
+++ management-project/loginproject/src/test/java/com/mangement/login/project/LoginprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mangement.login.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
