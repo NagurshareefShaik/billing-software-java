aaec63ee5c1d9794e106168f0df54080164edee2
nagubash <ngurshareef123@gmail.com>
2020-02-26 22:17:50 +0530

    patch-1 commit

--- management-project/loginproject/src/main/java/com/mangement/login/project/LoginprojectApplication.java
+++ management-project/loginproject/src/main/java/com/mangement/login/project/LoginprojectApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LoginprojectApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(LoginprojectApplication.class, args);
 	}
 
