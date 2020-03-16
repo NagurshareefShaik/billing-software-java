8ff56ef62c90dce344c820ef4ff0d72f9bd4b27f
nagubash <ngurshareef123@gmail.com>
2020-03-17 00:32:27 +0530

    db connection successuffly connected

--- billingsoftware/src/main/java/com/billingsoftware/controller/BillingController.java
+++ billingsoftware/src/main/java/com/billingsoftware/controller/BillingController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.billingsoftware.model.AddItems;
 import com.billingsoftware.repositorty.AddItemsRepo;
@@ -18,11 +19,24 @@ public class BillingController {
 	AddItemsRepo addItemsRepo;
 	
 	@RequestMapping(value = "/getData",method = RequestMethod.GET)
+	@ResponseBody
 	public List<AddItems> getAddItemData(){
 		System.out.println("***getData Method starts*******");
 		List<AddItems> listData=addItemsRepo.findAll();
 		System.out.println("***getData Method ends*******");
 		return listData;
 	}
+	@RequestMapping(value = "/insert",method = RequestMethod.POST)
+	public void insertData(AddItems addItems) {
+//		if(Objects.nonNull(addItems)) {
+//			addItemsRepo.save(addItems);
+//			return true;
+//		}
+//		else {
+//			return false;
+//		}
+		System.out.println("insert");
+	}
+	
 
 }
