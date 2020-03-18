44ad254f528cdff0fa8d50d4a9f661dacf41d263
Nagurshareef Shaik <42448704+NagurshareefShaik@users.noreply.github.com>
2020-03-18 23:04:30 +0530

    Billling software (#8)
    * db connection successuffly connected
    
    * save method introduced
    
    * add items data fetch completed
    
    Co-authored-by: nagubash <ngurshareef123@gmail.com>

--- billingsoftware/src/main/java/com/billingsoftware/entity/AddItemEntity.java
+++ billingsoftware/src/main/java/com/billingsoftware/entity/AddItemEntity.java
@@ -1,17 +1,14 @@
 package com.billingsoftware.entity;
 
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
+@Data
 @NoArgsConstructor
-@Getter
-@Setter
 public class AddItemEntity {
 	
-	private static Integer itemCode;
-	private static String itemName;
-	private static Integer itemPrice;
+	public  Integer itemCode;
+	public  String itemName;
+	public  Integer itemPrice;
 
 }
